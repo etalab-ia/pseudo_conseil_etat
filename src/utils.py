e25f1d78a9d03943db69a882ea294e93d731ac69
Pavel Soriano <pavel.soriano@data.gouv.fr>
2020-03-28 16:47:52 +0100

    added cpu stats to monitor

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 import GPUtil
 from threading import Thread
 import time
-
+import psutil
 class Monitor(Thread):
     def __init__(self, delay):
         super(Monitor, self).__init__()
@@ -11,7 +11,11 @@ class Monitor(Thread):
 
     def run(self):
         while not self.stopped:
-            GPUtil.showUtilization()
+            GPUtil.showUtilization(all=True)
+            print()
+            print(f"CPU usage (mean %): {psutil.cpu_percent()}")
+            print(f"System RAM usage (Gb): {dict(psutil.virtual_memory()._asdict())['used']/2.**30 }")
+            print()
             time.sleep(self.delay)
 
     def stop(self):
