4eab793806471f5fda12a8656a6ff7184b19f296
Pavel Soriano <pavel.soriano@data.gouv.fr>
2020-04-14 20:31:52 +0200

    new errors: under, over, miss, total. No more strict/flexible

--- src/results/evaluation_metier.py
+++ src/results/evaluation_metier.py
@@ -1,9 +1,17 @@
 '''
-This script calculates a metric "metier", that is, among (N) documents how many have a pseudonymisation error (d_e).
-M = d_e / N
-A pseudonymisation error can be strict or flexible. A strict error is when the golden differ with the predicted tag,
-no matter the predicted tag. A flexible error is when the golden tag is predicted as any entity, even if not the same
-(eg., B-PER_NOM predicted as I-PER_PRENOM)
+This script calculates a metric "metier", that is, among (N) documents how many have a pseudonymisation error (p_e).
+E = p_e / N
+A pseudonymisation error can be of three types: under, over, and miss anonymization.
+* Under anonymization is when an entity is marked as an entity by the hand annotated data (gold standard)
+and predicted to be not an entity. For example, B-NOM predicted as O.
+* Over anonymization is when an entity is marked as not an entity by the hand annotated data (gold standard)
+ and predicted to be an entity. For example, O predicted as B-PER.
+* Miss anonymization is when an entity is marked as an entity by the hand annotated data (gold standard)
+and also predicted to be an entity, but of different type (O). For example, B-NOM predicted as B-PRENOM.
+
+The metric is thus the number of documents that contain at least one of said errors. A fourth and final error is
+the number of any kind of error (amongst the last three) at least once in a document. This is a more strict metric
+as it considers all possible errors.
 
 The script takes one document with three columns, token, real tag, predicted tag
 Usage:
@@ -22,59 +30,83 @@ import pandas as pd
 
 def main(evaluation_conll_path):
     df_evaluation = pd.read_csv(evaluation_conll_path, names=["token", "true_tag", "pred_tag"],
-                                               delim_whitespace=True, engine="python",
-                                               skip_blank_lines=False).fillna("")
+                                delim_whitespace=True, engine="python",
+                                skip_blank_lines=False).fillna("")
     documents_tokens = [list(grp) for is_docstart, grp in groupby(df_evaluation["token"].iteritems(),
                                                                   lambda x: x[1] == "-DOCSTART-") if not is_docstart]
 
-    indices = []
-    list_not_identified = []
-    list_miss_identified = []
+    list_over_anonym = []
+    list_miss_anonym = []
+    list_under_anonym = []
+    list_all_anonym = []
     N = len(documents_tokens)
     results_dict = defaultdict(list)
     for doc_nb, document in enumerate(documents_tokens):
         indices = [i[0] for i in document]
         temp_df = df_evaluation.loc[indices]
-        not_identified_df = pd.DataFrame(temp_df[(temp_df["true_tag"] != temp_df["pred_tag"]) &
-                                        (temp_df["true_tag"] != "O")])
-        miss_identified_df = pd.DataFrame(temp_df[(temp_df["true_tag"] != temp_df["pred_tag"]) &
-                                         (temp_df["true_tag"] != "O") &
-                                         (temp_df["pred_tag"] != "O")])
 
+        under_anonym_df = pd.DataFrame(temp_df[(temp_df["pred_tag"] == "O") & (temp_df["true_tag"] != "O")])
+        over_anonym_df = pd.DataFrame(temp_df[(temp_df["true_tag"] == "O") & (temp_df["pred_tag"] != "O")])
+        miss_anonym_df = pd.DataFrame(temp_df[(temp_df["true_tag"] != "O") & (temp_df["pred_tag"] != "O") & (temp_df["true_tag"] != temp_df["pred_tag"])])
+        all_anonym_ids = set(under_anonym_df.index.to_list() + over_anonym_df.index.to_list() +
+                            miss_anonym_df.index.to_list())
 
+        under_anonym_df.loc[:, "errors"] = under_anonym_df["token"] + " : " + \
+                                           under_anonym_df["true_tag"] + "," + under_anonym_df["pred_tag"]
 
-        not_identified_df.loc[:, "errors"] = not_identified_df["token"] + " : " + \
-                                             not_identified_df["true_tag"] + "," + not_identified_df["pred_tag"]
-        miss_identified_df.loc[:, "errors"] = miss_identified_df["token"] + " : " + \
-                                              miss_identified_df["true_tag"] + "," + miss_identified_df["pred_tag"]
-        errors_not_identified = not_identified_df.errors.value_counts(sort=True).to_dict()
-        errors_miss_identified = miss_identified_df.errors.value_counts(sort=True).to_dict()
-        nb_not_identified = len(not_identified_df)
-        nb_miss_identified = len(miss_identified_df)
-        if nb_not_identified:
-            results_dict["doc_nb"].append(doc_nb)
-            results_dict["nb_not_identified"].append(nb_not_identified)
-            results_dict["nb_miss_identified"].append(nb_miss_identified)
-            results_dict["not_identified_frequency"].append(errors_not_identified)
-            results_dict["miss_identified_frequency"].append(errors_miss_identified)
+        over_anonym_df.loc[:, "errors"] = over_anonym_df["token"] + " : " + \
+                                          over_anonym_df["true_tag"] + "," + over_anonym_df["pred_tag"]
 
-        list_not_identified.append(nb_not_identified)
-        list_miss_identified.append(nb_miss_identified)
+        miss_anonym_df.loc[:, "errors"] = miss_anonym_df["token"] + " : " + \
+                                          miss_anonym_df["true_tag"] + "," + miss_anonym_df["pred_tag"]
 
+        under_anonym_freqs = under_anonym_df.errors.value_counts(sort=True).to_dict()
+        over_anonym_freqs = over_anonym_df.errors.value_counts(sort=True).to_dict()
+        miss_anonym_freqs = miss_anonym_df.errors.value_counts(sort=True).to_dict()
 
-    strict_error = sum(1 for n in list_not_identified if n)
-    flexible_error = strict_error - sum(1 for n in list_miss_identified if n)
+        nb_under_anonym_errors = len(under_anonym_df)
+        nb_over_anonym_errors = len(over_anonym_df)
+        nb_miss_anonym_errors = len(miss_anonym_df)
+        nb_all_errors = len(all_anonym_ids)
 
-    strict_error /= N
-    flexible_error /= N
-    return strict_error, flexible_error, list_not_identified, list_miss_identified, results_dict
+        if not sum([nb_under_anonym_errors, nb_over_anonym_errors, nb_miss_anonym_errors]):
+            continue
+
+        results_dict["doc_nb"].append(doc_nb)
+        for error_name, error_count, errors_instances in [("nb_under_anonym_errors", nb_under_anonym_errors,
+                                                           under_anonym_freqs),
+                                        ("nb_over_anonym_errors", nb_over_anonym_errors, over_anonym_freqs),
+                                        ("nb_miss_anonym_errors", nb_miss_anonym_errors, miss_anonym_freqs)]:
+            results_dict[error_name].append(error_count)
+            results_dict[f"{error_name[3:]}_frequencies"].append(errors_instances)
+
+        list_under_anonym.append(nb_under_anonym_errors)
+        list_over_anonym.append(nb_over_anonym_errors)
+        list_miss_anonym.append(nb_miss_anonym_errors)
+        list_all_anonym.append(nb_all_errors)
+
+    under_anonym_error = sum(1 for n in list_under_anonym if n) / N
+    over_anonym_error = sum(1 for n in list_over_anonym if n) / N
+    miss_anonym_error = sum(1 for n in list_miss_anonym if n) / N
+    all_anonym_error = sum(1 for n in list_all_anonym if n) / N
+
+    return under_anonym_error, over_anonym_error, miss_anonym_error, all_anonym_error, \
+           list_under_anonym, list_over_anonym, list_miss_anonym,\
+           results_dict
 
 
 if __name__ == '__main__':
     parser = argopt(__doc__).parse_args()
     evaluation_conll_path = Path(parser.evaluation_conll_path)
 
-    strict_error, flexible_error, _, _, results_dict = main(evaluation_conll_path=evaluation_conll_path)
+    all_results = main(evaluation_conll_path=evaluation_conll_path)
+
+    under_anonym_error = all_results[0]
+    over_anonym_error = all_results[1]
+    miss_anonnym_error = all_results[2]
+    all_anonym_error = all_results[3]
+    results_dict = all_results[7]
+
     results_df = pd.DataFrame.from_dict(results_dict)
 
     pd.set_option('display.max_columns', None)
@@ -83,15 +115,29 @@ if __name__ == '__main__':
     print()
     for doc_nb, row in results_df.iterrows():
         print(f"Doc nb: {row['doc_nb']}")
-        print(f"\tNb Not Identified: {row['nb_not_identified']}")
-        print(f"\tNb Miss Identified    : {row['nb_miss_identified']}")
-        print("\tNot Identified Errors:")
-        for error, freq in row["not_identified_frequency"].items():
+        print(f"\tNb Under Anonyimized: {row['nb_under_anonym_errors']}")
+        print(f"\tNb Miss Anonymized: {row['nb_miss_anonym_errors']}")
+        print(f"\tNb Over Anonymized: {row['nb_over_anonym_errors']}")
+
+        print("\tUnder Anonymized Errors:")
+        for error, freq in row["under_anonym_errors_frequencies"].items():
             print(f"\t\t{error}: {freq}")
-        print("\tMiss Identified Errors:")
-        for error, freq in row["miss_identified_frequency"].items():
+
+        print("\tMiss Anonymized Errors:")
+        for error, freq in row["miss_anonym_errors_frequencies"].items():
             print(f"\t\t{error}: {freq}")
-    # print(results_df)
+
+        print("\tOver Anonymized Errors:")
+        for error, freq in row["over_anonym_errors_frequencies"].items():
+            print(f"\t\t{error}: {freq}")
+
+
+
     print()
-    print(f"The total strict error (predicted label differs from golden label) is : {strict_error}")
-    print(f"The total flexible error (predicted label is not correct but still identified) is: {flexible_error}")
+    print(f"The under anonymization error (not anonymizing an annotated entity (PRENOM as O)) is: "
+          f" {under_anonym_error:.2f}")
+    print(f"The miss anonymization error (not anonymizing correctly an entity (NOM as PRENOM)) is: "
+          f" {miss_anonnym_error:.2f}")
+    print(f"The over anonymization error (assigning a tag to non entities (O as NOM)) is:   "
+          f"{over_anonym_error:.2f}")
+    print(f"The total anonymization error (any kind of error) is: {all_anonym_error:.2f}")
